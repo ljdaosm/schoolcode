e287d7517325563459d19965638b6b6926b3de3b
ljdaosm <39611504+ljdaosm@users.noreply.github.com>
2019-07-10 10:47:55 +0530

    Add files via upload

---
+++ Q6/Q6.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+labels=['Uttar Pradesh','West bengal','Rajasthan','Himachal Pradesh','Madhya Pradesh','Tamil Nadu','Kerala','Assam','Karnataka','Bihar']
+x=[828,1029,200,123,240,550,860,397,320,1102]
+plt.pie(x,labels=labels,shadow=True)
+plt.show()
