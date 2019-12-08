989a86c2d98c34293724144365fb9c4a06861473
ljdaosm <39611504+ljdaosm@users.noreply.github.com>
2019-12-08 18:34:03 +0530

    Update Q14.py

--- Q14/Q14.py
+++ Q14/Q14.py
@@ -1,21 +1,18 @@
-f = open("data.txt", "r")
-l = f.readlines()
-for i in range(len(l)):
-    l[i] = tuple(l[i].split())
+f=open("q14.txt")
+l1=[]
 
-def sortNew(n):
-    return n[2]
-l.sort(key = sortNew)
-
-d = {}
-print("People with Experience less than 3 years:")
-for i in l:
-    if int(i[3]) < 3:
-        print(i[0], i[1])
-    if i[4] in d:
-        d[i[4]] += 1
-    else:
-        d[i[4]] = 1
+a=f.readlines()
+for i in range(0,len(a)):
+    for word in str(i):
+        x=a[i].split()
+        l1.append(tuple(x))
+print(l1)
 print()
-for i in d.items():
-    print(i[0],":",i[1])
+print()
+
+for tup in l1:
+    y=int(tup[3])
+    if y<3:
+        print(tup[0],tup[1],":no of years:",y)
+    
+
